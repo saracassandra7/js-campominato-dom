d8b632836863d90084cc4324274eeddb20a9225b
saracassandra7 <saracassandra7@gmail.com>
2022-10-21 18:24:34 +0200

    [add] corrections and created bombs

--- js/script.js
+++ js/script.js
@@ -9,32 +9,35 @@ La partita termina quando il giocatore clicca su una bomba o raggiunge il numero
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 1) fare la griglia, che si trova nel game wrapper, compare al click di play ed ha una dimensione diversa in base al livello
-2)una volta generata la griglia, gener le celle. Ognuna dovrà avere un ID */
+2)una volta generata la griglia, gener le celle. Ognuna dovrà avere un ID
+3) generare le bombe */
 
 const main = document.querySelector('.game-wrapper');
 const playBtn = document.querySelector('#play');
 const levelSelect = document.querySelector('#level');
 
 const gridLevels = [100, 81, 49];
-
+const BOMBS_NUMBER = 16;
+let bombs = [];
 
 playBtn.addEventListener('click', play);
 
 function play(){
   //in base al value stabilisco in numero di celle (cambia in base al livello scelto);
   const cellNumbers = gridLevels[levelSelect.value];
-
   reset();
 
   //genero la griglia tramite una funzione
   generatePlayGround(cellNumbers);
+
+  //genero le bombe
+  bombs = generateBombs(cellNumbers);
 }
 
 function generatePlayGround(cellNumbers){
   //creo la griglia e gli aggiungo la classe
   const grid = document.createElement('div');
   grid.className = 'grid';
-
   //genero le celle
   for(let i = 1; i <= cellNumbers; i++){
     const cell = generateCell(i, cellNumbers);
@@ -54,6 +57,29 @@ function generateCell (cellId, cellNumbers){
   cell.cellId = cellId;
   cell.innerHTML = ` <span>${cellId}</span>`;
   return cell;
+}
+
+//funzione che genera le bombe
+function generateBombs(cellNumbers){
+  //genero gli Id delle bombe e li salvo in un array che restituisco
+  const bombsGenerated = [];
+
+  //ciclo fino a quando non ho generato il numero di bombe necessarie
+  while(bombsGenerated.length<BOMBS_NUMBER){
+    const bomb = generateRandomNumber(1, cellNumbers); //funzione che genera bombe tramite numero random
+
+    // se il numero è presente nell'array di bombe, lo pusho
+    if(!bombsGenerated.includes(bomb)){
+      bombsGenerated.push(bomb);
+    }
+  }
+  console.log('bombe', bombsGenerated);
+  return bombsGenerated;
+}
+
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random()*(max - min + 1)) + min;
 }
 
 function reset() {
